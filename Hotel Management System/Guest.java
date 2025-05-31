c0cf7c4cfe953c64d8012a5ac701f3db5478c768
Nayem Uddin <nayemuddin2006@gmail.com>
2025-05-31 10:11:36 +0600

    Add files via upload

---
+++ Hotel Management System/Guest.java
@@ -0,0 +1,20 @@
+
+package HotelManagementSystem;
+
+public class Guest {
+    
+    private String name;
+    private String phone;
+
+    public Guest(String name, String phone) {
+        this.name = name;
+        this.phone = phone;
+    }
+
+    public String getName() { 
+        return name; 
+    }
+    
+}
+
+   
---
+++ Hotel Management System/Hotel.java
@@ -0,0 +1,24 @@
+
+package HotelManagementSystem;
+
+
+public class Hotel {
+    
+    private String name;
+    private String location;
+
+    public Hotel(String name, String location) {
+        this.name = name;
+        this.location = location;
+    }
+
+    public String getName() { 
+        return name; 
+    }
+    
+    public String getLocation() {
+        return location; 
+    }
+    
+}
+
